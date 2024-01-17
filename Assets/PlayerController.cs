e0df06af7e8e65680f58bbb0dd6eb1c2d0a2a571
Albaryys <6205@holbertonstudents.com>
2024-01-17 16:59:43 +0100

    add task 1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody playerBody;
+    public float speed = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    // Update the movement of the player
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        playerBody.AddForce(movement * Time.deltaTime * speed);
+    }
+}
